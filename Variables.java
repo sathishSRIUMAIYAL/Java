85df0339e765f7a868619f0a14de1c85ae53d94d
sathishSRIUMAIYAL <sathishc439@gmail.com>
2024-10-20 20:18:25 +0530

    Create New Variables

---
+++ Variables.java
@@ -0,0 +1,18 @@
+// Variables Are containers for storing data types.
+
+// To craete a variable you must specify the type and assign it a value .
+
+// Data type ("String", "int", "boolean", "long") 
+
+public class Variables{
+    public static void main(String[] args) {
+
+        String Name="Sathishlumar";   // Variable one 
+        int Age=26;                   // Variable two
+        boolean Married=false;        // Variable three
+
+        System.out.println(Name);
+        System.out.println(Age);
+        System.out.println(Married);
+    }
+}
